f550ce24b633873478142d28fb660e28a7806127
zongzhiping@imassbank.com <zongzhiping@imassbank.com>
2017-06-25 16:50:27 +0800

    a

--- src/main/java/com/imooc/quartz/HelloJob.java
+++ src/main/java/com/imooc/quartz/HelloJob.java
@@ -1,8 +1,6 @@
 package com.imooc.quartz;
 
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
+import org.quartz.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,10 +9,17 @@ import java.util.Date;
  * Created by zzy on 2017/6/20.
  */
 public class HelloJob implements Job {
-    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        try {
+            Thread.sleep(2000l);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Date date = new Date();
         SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println("The Curr Time is: "+sf.format(date));
+        System.out.println("The Exec Time is: "+sf.format(date));
         System.out.println("Hello World!");
+
     }
 }
--- src/main/java/com/imooc/quartz/HelloScheduler.java
+++ src/main/java/com/imooc/quartz/HelloScheduler.java
@@ -11,24 +11,37 @@ import java.util.Date;
  */
 public class HelloScheduler {
     public static void main(String args[]) throws SchedulerException {
-        JobDetail jobDetail = JobBuilder.newJob(HelloJob.class).withIdentity("myJob","group1").build();
-        Trigger trigger = TriggerBuilder.newTrigger()
-                .withIdentity("myTriger","group1")
-                .startNow()
+        Date date = new Date();
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("The Curr Time is: " + sf.format(date));
+        date.setTime(date.getTime() + 3000);
+
+        Date endTime = new Date();
+        endTime.setTime(endTime.getTime() + 6000);
+
+        JobDetail jobDetail = JobBuilder.newJob(HelloJob.class)
+                .withIdentity("myJob", "group1")
+                .build();
+        CronTrigger trigger = TriggerBuilder.newTrigger()
+                .withIdentity("myTriger", "group1")
                 .withSchedule(
-                        SimpleScheduleBuilder
-                        .simpleSchedule()
-                        .withIntervalInSeconds(2)
-                        .repeatForever())
+                       CronScheduleBuilder.cronSchedule("* * * * * ?"))
                 .build();
 
         SchedulerFactory schedulerFactory = new StdSchedulerFactory();
         Scheduler scheduler = schedulerFactory.getScheduler();
         scheduler.start();
-        Date date = new Date();
-        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.println("The Exec Time is: "+sf.format(date));
-        scheduler.scheduleJob(jobDetail,trigger);
+        scheduler.scheduleJob(jobDetail, trigger);
+
+        try {
+            Thread.sleep(1000l);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        scheduler.shutdown(true);
+
+        System.out.println(scheduler.isShutdown());
 
     }
 
